05df5bc12999f78c68bca6ae7311fe46f3791aa9
boikanyobaitshepi <boikanyobaitshepi@gmail.com>
2024-06-27 07:12:46 +0200

    "Updated index.html to replace weather app content with navigation menu and added corresponding styles in styles.css"

---
+++ index.js
@@ -0,0 +1,13 @@
+// const api = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`;
+// const city = 'London';
+// const apiKey = 'd84fde2f47ee96ab177a90116b5647ee';
+
+// fetch(`${api}?q=${city}&appid=${apiKey}`)
+//   .then(response => response.json())
+//   .then(data => {
+//     const temperature = data.main.temp;
+//     const description = data.weather[0].description;
+//     document.getElementById('city').innerText = city;
+//     document.getElementById('temperature').innerText = `${temperature}°C`;
+//     document.getElementById('description').innerText = description;
+//   });
